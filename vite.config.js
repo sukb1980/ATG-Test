fdbb77aa2727f2176018d7725a4d2b9fb45c736d
sukb1980 <60866953+sukb1980@users.noreply.github.com>
2025-12-24 15:29:23 +0530

    Changed to PWA

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,34 @@
+import { VitePWA } from "vite-plugin-pwa"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'ATG Employee Engagement',
+        short_name: 'ATG App',
+        description: 'Employee engagement platform',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   base: '/ATG-Test/',
 })
